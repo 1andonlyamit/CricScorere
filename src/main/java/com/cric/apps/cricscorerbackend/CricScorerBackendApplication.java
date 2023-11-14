e01cf382d534382bd3a3c96091dc6968bd20da59
Amit <amitmakar98602@gmail.com>
2023-11-14 11:25:45 +0530

    project CricScorer Started

---
+++ src/main/java/com/cric/apps/cricscorerbackend/CricScorerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cric.apps.cricscorerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricScorerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CricScorerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cric/apps/cricscorerbackend/CricScorerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cric.apps.cricscorerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricScorerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
